cab26b41882a26de9d711a2e60f7dba136bca360
Joshua Masci <Iamshortman@gmail.com>
2018-05-07 00:22:07 -0700

    basic json loading now working

--- src/Client/GameState/GameState_Singleplayer.cpp
+++ src/Client/GameState/GameState_Singleplayer.cpp
@@ -15,11 +15,11 @@ GameState_Singleplayer::GameState_Singleplayer()
 	this->scene_root = GameObjectManager::getInstance()->createGameObject();
 	this->scene_root->addComponent<PhysicsWorld>();
 
-	GameObject* ship = GameObjectManager::getInstance()->createGameObject();
-	ship->setLocalTransform(Transform( vector3D(0.0, 0.0, 0.0), glm::angleAxis(glm::radians(90.0), vector3D(0.0, 1.0, 0.0) )));
-	this->scene_root->addChild(ship);
-
-	ship->addComponent<ComponentModel>("res/models/Cobra/Hull.obj", "res/textures/1K_Grid.png", "Textured", "Textured_Lighting", "Textured_Shadow");
+	//GameObject* ship = GameObjectManager::getInstance()->createGameObject();
+	//ship->setLocalTransform(Transform( vector3D(0.0, 0.0, 0.0), glm::angleAxis(glm::radians(90.0), vector3D(0.0, 1.0, 0.0) )));
+	//ship->addComponent<ComponentModel>("res/models/Cobra/Hull.obj", "res/textures/1K_Grid.png", "Textured", "Textured_Lighting", "Textured_Shadow");
+	//this->scene_root->addChild(ship);
+	this->scene_root->addChild(GameObjectManager::getInstance()->createGameObjectFromJson("res/json/test.json"));
 
 	GameObject* camera = GameObjectManager::getInstance()->createGameObject();
 	this->scene_root->addChild(camera);
--- src/Common/GameObjectManager.cpp
+++ src/Common/GameObjectManager.cpp
@@ -1,5 +1,7 @@
 #include "Common/GameObjectManager.hpp"
 
+
+#include "Common/Component/ComponentModel.hpp"
 #include "jsoncons/json.hpp"
 using jsoncons::json;
 
@@ -57,7 +59,7 @@ GameObject* GameObjectManager::createGameObject()
 	this->game_objects[game_object_id] = game_object;
 	return game_object;
 }
-
+ 
 GameObject* GameObjectManager::getGameObject(GameObjectId game_object_id)
 {
 	if (this->game_objects.find(game_object_id) == this->game_objects.end())
@@ -73,13 +75,63 @@ void GameObjectManager::destroyGameObject(GameObjectId game_object_id)
 	this->game_objects_to_delete.push(game_object_id);
 }
 
-GameObject * GameObjectManager::createGameObjectFromJson(string file)
+void parseComponents(GameObject* game_object, json json_component)
+{
+	if (json_component.has_member("model"))
+	{
+		json json_model = json_component["model"];
+		//string mesh, string texture, string ambient_shader
+		string file_path = json_model["file_path"].as_string();
+		string texture = json_model["texture"].as_string();
+		string ambient_shader = json_model["ambient_shader"].as_string();
+		game_object->addComponent<ComponentModel>(file_path, texture, ambient_shader, "", "");
+	}
+}
+
+GameObject* parseGameObject(json json_game_object)
+{
+	GameObject* game_object = GameObjectManager::getInstance()->createGameObject();
+
+	if (json_game_object.has_member("transform"))
+	{
+		Transform transform = game_object->getLocalTransform();
+		json json_transform = json_game_object["transform"];
+
+		if (json_transform.has_member("position"))
+		{
+			vector<double> pos = json_transform["position"].as<vector<double>>();
+			transform.setPosition(vector3D(pos[0], pos[1], pos[2]));
+		}
+		
+		if (json_transform.has_member("orientation"))
+		{
+			vector<double> pos = json_transform["orientation"].as<vector<double>>();
+			transform.setOrientation(quaternionD( pos[0], pos[1], pos[2], pos[3]));
+		}
+
+		game_object->setLocalTransform(transform);
+	}
+
+	if (json_game_object.has_member("components"))
+	{
+		json components = json_game_object["components"];
+		parseComponents(game_object, components);
+	}
+
+	if (json_game_object.has_member("children"))
+	{
+		json children = json_game_object["children"];
+		for (int i = 0; i < children.size(); i++)
+		{
+			game_object->addChild(parseGameObject(children[i]));
+		}
+	}
+
+	return game_object;
+}
+
+GameObject* GameObjectManager::createGameObjectFromJson(string file)
 {
 	json json_file = json::parse_file(file);
-
-	//if(json_file)
-
-
-
-	return nullptr;
+	return parseGameObject(json_file);
 }
