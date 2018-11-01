60cf272a7261dbb2626d66454f0ddf80311e00c3
Iamshortman <Iamshortman@gmail.com>
2018-11-01 16:58:43 -0700

    Remove Useless code

--- src/Client/Rendering/Camera.cpp
+++ src/Client/Rendering/Camera.cpp
@@ -11,16 +11,12 @@ matrix4 Camera::getProjectionMatrix(int screenWidth, int screenHeight)
 {
 	float aspect_ratio = ((float)screenWidth) / ((float)screenHeight);
 
-	float fovy = 2 * atan(tan(this->frame_of_view / 2) / aspect_ratio);
-
-	matrix4 projection_matrix = glm::tweakedInfinitePerspective(fovy, aspect_ratio, this->z_near);
-
 	float f = 1.0f / tan(glm::radians(this->frame_of_view) / 2.0f);
 	return glm::mat4(
-		f / aspect_ratio, 0.0f, 0.0f, 0.0f,
-		0.0f, f, 0.0f, 0.0f,
-		0.0f, 0.0f, 0.0f, -1.0f,
-		0.0f, 0.0f, this->z_near, 0.0f);
+		f / aspect_ratio, 	0.0f, 	0.0f, 			0.0f,
+		0.0f, 				f, 		0.0f,			0.0f,
+		0.0f, 				0.0f, 	0.0f, 			-1.0f,
+		0.0f, 				0.0f, 	this->z_near, 	0.0f);
 }
 
 matrix4 Camera::getProjectionMatrix(vector2I& screen_size)
